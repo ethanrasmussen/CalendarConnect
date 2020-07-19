2941e240d0087c4a1d3c2c15476e260b0d3fc384
ethanrasmussen <itseraz@gmail.com>
2020-07-19 07:42:03 -0500

    add discordstatusupdater

--- cc_main.py
+++ cc_main.py
@@ -38,3 +38,59 @@ class TeamupCalendar:
 
 # TODO: MS Calendar
 # TODO: Google Calendar
+
+class DiscordStatusUpdater:
+
+    def __init__(self, events:List[StatusEvent], discord_email:str, discord_password:str):
+
+        # init variables
+        self.events = events
+        self.dis_email = discord_email
+        self.dis_password = discord_password
+
+        # touch base with Discord servers and obtain 'fingerprint'
+        self.fingerprint = fingerprint = ast.literal_eval(requests.post(url="https://discord.com/api/v6/auth/fingerprint").content.decode("UTF-8"))['fingerprint']
+
+        # begin session
+        self.s = requests.Session()
+        r = self.s.get(url="https://discord.com/channels/@me", headers={
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
+        })
+
+        # login
+        r_login = self.s.post(url="https://discord.com/api/v6/auth/login", headers={
+            "content-type": "application/json",
+            "origin": "https://discord.com",
+            "referer": "https://discord.com/login?redirect_to=%2Fchannels%2F%40me",
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36",
+            "x-fingerprint": fingerprint,
+            "x-super-properties": "eyJvcyI6IldpbmRvd3MiLCJicm93c2VyIjoiQ2hyb21lIiwiZGV2aWNlIjoiIiwiYnJvd3Nlcl91c2VyX2FnZW50IjoiTW96aWxsYS81LjAgKFdpbmRvd3MgTlQgMTAuMDsgV2luNjQ7IHg2NCkgQXBwbGVXZWJLaXQvNTM3LjM2IChLSFRNTCwgbGlrZSBHZWNrbykgQ2hyb21lLzgzLjAuNDEwMy4xMTYgU2FmYXJpLzUzNy4zNiIsImJyb3dzZXJfdmVyc2lvbiI6IjgzLjAuNDEwMy4xMTYiLCJvc192ZXJzaW9uIjoiMTAiLCJyZWZlcnJlciI6Imh0dHBzOi8vZGlzY29yZC5jb20vY2hhbm5lbHMvQG1lIiwicmVmZXJyaW5nX2RvbWFpbiI6ImRpc2NvcmQuY29tIiwicmVmZXJyZXJfY3VycmVudCI6IiIsInJlZmVycmluZ19kb21haW5fY3VycmVudCI6IiIsInJlbGVhc2VfY2hhbm5lbCI6InN0YWJsZSIsImNsaWVudF9idWlsZF9udW1iZXIiOjYzNjAxLCJjbGllbnRfZXZlbnRfc291cmNlIjpudWxsfQ=="
+        }, data=json.dumps({
+            "captcha_key": None,
+            "email": self.dis_email,
+            "gift_code_sku_id": None,
+            "login_source": None,
+            "password": self.dis_password,
+            "undelete": False
+        }))
+        # get token
+        self.token = ast.literal_eval(r_login.content.decode("UTF-8"))['token']
+
+
+    def daily_schedule_update(self):
+        for event in self.events:
+            # get the timestamp for 1 minute before event ends
+            today = datetime.datetime.today()
+            event_end = (datetime.datetime(today.year, today.month, today.day, hour=int(event.end.split(':')[0]), minute=int(event.end.split(':')[1])) + datetime.timedelta(minutes=-1)).time().strftime('%H:%M')
+            # schedule a discord status update to begin
+            schedule.every().day.at(event.start).do(dsu.update_status, status=event.name, session=self.s, fingerprint=self.fingerprint, token=self.token)
+            # schedule the discord status to clear 1 minute before event end
+            schedule.every().day.at(event_end).do(dsu.clear_status, session=self.s, fingerprint=self.fingerprint, token=self.token)
+
+
+
+# get calendar and create status updater
+def status_updates(calendar:str, email:str, password:str):
+    cal = TeamupCalendar(calendar)
+    events = cal.get_events()
+    update = DiscordStatusUpdater(events, email, password)
